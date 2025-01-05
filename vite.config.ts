50db60e4c68a321d63325c9ed4d60630b35d2290
Caracol <rafaelfariasm@live.com>
2025-01-04 21:29:02 -0300

    added pwa suport

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,52 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/prote-ja-app/",
 
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "mask-icon.svg",
+      ],
+      manifest: {
+        theme_color: "#8168ff",
+        background_color: "#8168ff",
+        icons: [
+          {
+            src: "icon192_maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "icon512_maskable.png",
+            type: "image/png",
+          },
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "icon512_rounded.png",
+            type: "image/png",
+          },
+        ],
+        orientation: "portrait",
+        display: "standalone",
+        lang: "pt-BR",
+        name: "ProteJÁ",
+        description: "Aplicativo ProteJÁ",
+      },
+    }),
+  ],
 
   resolve: {
     alias: {
