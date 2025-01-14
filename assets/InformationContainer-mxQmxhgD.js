aced8d3c88b4235b3b138feafebc4f4dbf291cdf
Reinaldo-Kn <Reinaldo-Kn@users.noreply.github.com>
2025-01-14 17:11:14 +0000

    deploy: 3232f447e492dab0b81571d1e8bd1b22addc2f0a

---
+++ assets/InformationContainer-mxQmxhgD.js
@@ -0,0 +1,6 @@
+import{c as l,j as a,b as n,B as r}from"./index-BF73LbLp.js";/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=l("Settings",[["path",{d:"M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z",key:"1qme2f"}],["circle",{cx:"12",cy:"12",r:"3",key:"1v7zrd"}]]),x=({onEdit:e,name:s,value:t})=>a.jsx(n,{square:!0,children:a.jsxs("div",{className:"flex justify-between items-center text-white py-2 px-3",children:[a.jsx("p",{className:"text-lg font-medium",children:s}),a.jsxs("div",{className:"text-lg flex gap-4 items-center",children:[t,e&&a.jsx(r,{onClick:e,className:"[&_svg]:size-5",size:"icon",variant:"secondary",children:a.jsx(i,{})})]})]})});export{x as I,i as S};
---
+++ assets/Pairing-BqPXmebt.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BF73LbLp.js";import"./react-DxTR-FPg.js";import"./react-dom-DOp_neaj.js";const n=()=>r.jsx("div",{children:"Pairing"});export{n as default};
