328063858d483bce7d46780c61ef7c2106d15850
Caracol <rafaelfariasm@live.com>
2025-01-18 07:40:29 -0300

    added menu to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,27 @@ import { FunctionComponent, useEffect, useState } from "react";
 import ProtejaLogo from "@/assets/proteja_logo.svg";
 import ProtejaName from "@/assets/proteja_name.svg";
 import { Button } from "./ui/button";
-import { Bell, User, ChevronLeft, Download } from "lucide-react";
+import { Bell, User, ChevronLeft, Download, Menu, LogOut } from "lucide-react";
 import { Link, useLocation } from "react-router";
 import { useAuth } from "@/hooks/useAuth";
 import { cn } from "@/lib/utils";
 import { usePWAInstall } from "react-use-pwa-install";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { getFirstName } from "@/lib/helpers";
+import { signOut } from "@/db/auth";
+import { toast } from "react-toastify";
 
 interface HeaderProps {}
 
 const Header: FunctionComponent<HeaderProps> = () => {
-  const { session } = useAuth();
+  const { session, user } = useAuth();
   const location = useLocation();
   const [showBackButton, setShowBackButton] = useState(false);
   const install = usePWAInstall();
@@ -24,6 +35,16 @@ const Header: FunctionComponent<HeaderProps> = () => {
       setShowBackButton(false);
     }
   }, [location.pathname, session]);
+
+  const handleSignOut = async () => {
+    const res = await signOut();
+
+    if (res.error) {
+      console.error("Error signing out: ", res.error);
+      toast.error("Erro ao sair. Tente novamente.");
+      return;
+    }
+  };
 
   return (
     <div className="border-b flex items-center sticky top-0 z-10 backdrop-blur md:shadow">
@@ -86,45 +107,69 @@ const Header: FunctionComponent<HeaderProps> = () => {
 
         {/* Right Section */}
         <div className="gap-2 flex items-center ml-auto">
+          {install ? (
+            <Button
+              variant={"secondary"}
+              className="w-12 h-12 md:w-auto md:h-auto [&_svg]:size-5 border-white"
+              onClick={install}
+            >
+              <Download />
+              <span className="hidden md:block">Instalar</span>
+            </Button>
+          ) : null}
           {session ? (
             <>
-              {install ? (
-                <Button
-                  variant={"secondary"}
-                  className="w-12 h-12 md:w-auto md:h-auto [&_svg]:size-5"
-                  onClick={install}
-                >
-                  <Download />
-                  <span className="hidden md:block">Download</span>
-                </Button>
-              ) : null}
-              <Link to="/dashboard/subscribe">
-                <Button
-                  variant={"secondary"}
-                  className="w-12 h-12 md:w-auto md:h-auto py-2 px-2 md:px-4"
-                >
-                  <img src={ProtejaLogo} className="h-6 md:h-5" alt="" />
-                  <span className="hidden md:block">ProteJÁ Plus</span>
-                </Button>
-              </Link>
-              <Link to="/dashboard/profile">
-                <Button
-                  variant={"secondary"}
-                  className="w-12 h-12 md:w-auto md:h-auto [&_svg]:size-5"
-                >
-                  <User />
-                  <span className="hidden md:block">Meu Perfil</span>
-                </Button>
-              </Link>
               <Link to="/dashboard/alerts">
                 <Button
                   variant={"secondary"}
                   className="w-12 h-12 md:w-auto md:h-auto [&_svg]:size-5"
                 >
-                  <Bell />
+                  <Bell className="fill-white" />
                   <span className="hidden md:block">Notificações</span>
                 </Button>
               </Link>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button
+                    variant={"secondary"}
+                    className="w-12 h-12 md:w-auto md:h-auto [&_svg]:size-5"
+                  >
+                    <Menu />
+                    <span className="hidden md:block">Menu</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>
+                    Olá {getFirstName(user?.name)}
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <Link to="/dashboard/profile">
+                    <DropdownMenuItem>
+                      <User /> Meu Perfil
+                    </DropdownMenuItem>
+                  </Link>
+                  <Link to="/dashboard/subscribe">
+                    <DropdownMenuItem>
+                      <img
+                        src={ProtejaLogo}
+                        className="h-[1.15rem] px-[0.025rem] invert"
+                        alt=""
+                      />
+                      ProteJÁ+
+                    </DropdownMenuItem>
+                  </Link>
+
+                  <DropdownMenuSeparator />
+
+                  <DropdownMenuItem
+                    className="text-destructive font-medium"
+                    onClick={handleSignOut}
+                  >
+                    <LogOut />
+                    Sair
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </>
           ) : null}
         </div>
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -31,3 +31,7 @@ export function checkValidMac(mac: string): boolean {
   const macRegex = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
   return macRegex.test(mac);
 }
+
+export function getFirstName(name?: string) {
+  return name?.split(" ")[0];
+}
