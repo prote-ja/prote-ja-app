46569131e114134e98c9801699528e536999bd43
zRafaF <zRafaF@users.noreply.github.com>
2025-02-02 20:27:48 +0000

    deploy: 67e923463d03c39b0959c47dc6dd8338cd73a98a

--- assets/AlertComponent-BIUwdrsI.js
+++
@@ -1,6 +0,0 @@
-import{h as n,j as e,e as x,d as h}from"./index-DD037din.js";import{A as m,a as o,b as d}from"./alert-Bzm0jT0O.js";/**
- * @license lucide-react v0.468.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const y=n("CircleAlert",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]]),f=({variant:l="warning",iconTitle:s,title:a,description:c,buttonLabel:t,buttonRedirectUrl:i,iconButton:r})=>e.jsxs(m,{variant:l,className:"flex justify-between items-center text-white py-2 px-4 gap-4",children:[e.jsxs("div",{className:"flex items-center gap-4",children:[s&&e.jsx(s,{className:"h-7 w-7 flex-shrink-0 stroke-white"}),e.jsxs("div",{children:[e.jsx(o,{className:"text-lg  font-semibold",children:a}),e.jsx(d,{className:"text-sm",children:c})]})]}),t&&i&&e.jsx(x,{to:i,children:e.jsxs(h,{variant:"outline",className:" text-white border-white hover:bg-black/10  flex items",children:[t,r&&e.jsx(r,{className:"h-5 w-5 flex-shrink-0 stroke-white "})]})})]});export{f as A,y as C};
--- assets/TotemsPairing-CkKfwo5e.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DD037din.js";import"./react-DDCWQfk_.js";import"./react-dom-DZBigVa_.js";const s=()=>r.jsx("div",{children:"Pairing"});export{s as default};
