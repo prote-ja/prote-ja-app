a73b6ca2de1ed71143b3974ea1e7b6da1624f8e6
Caracol <rafaelfariasm@live.com>
2025-04-19 15:32:28 -0300

    Updates

---
+++ assets/TotemsView-CDGo0zjt.js
@@ -0,0 +1,1 @@
+import{a2 as t,j as s}from"./index-DNAhENDT.js";import"./react-C7sMpjI8.js";import"./react-dom-ThsgQk-A.js";const a=()=>{const{id:r}=t();return s.jsxs("div",{children:[s.jsx("h1",{children:"View"}),r]})};export{a as default};
